87b84be632bb648bda1ac6311da44ce8f97705f1
VladimirAdamiya <adamiya.v@gmail.com>
2021-04-24 15:43:04 -0500

    initial commit

--- src/Day1/HelloWorld.java
+++ src/Day1/HelloWorld.java
@@ -5,5 +5,6 @@ import java.sql.SQLOutput;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Adding GitHub Now");
     }
 }
